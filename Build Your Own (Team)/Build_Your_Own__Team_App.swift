4db2c832127184888751e113a8fdd6bf0828c82c
Ethan Davis <edavis@bsd220.org>
2024-04-12 09:34:36 -0500

    Initial Commit

---
+++ Build Your Own (Team)/Build_Your_Own__Team_App.swift
@@ -0,0 +1,17 @@
+//
+//  Build_Your_Own__Team_App.swift
+//  Build Your Own (Team)
+//
+//  Created by Ethan Davis on 4/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Build_Your_Own__Team_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Build Your Own (Team)/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Build Your Own (Team)
+//
+//  Created by Ethan Davis on 4/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
