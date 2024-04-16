7f2bad0b0fc6d843c19d0adf2b41d978f99f4ca6
Ethan Davis <edavis@bsd220.org>
2024-04-16 09:33:59 -0500

    got link from title screen to main game working

---
+++ Build Your Own (Team)/StartView.swift
@@ -0,0 +1,18 @@
+//
+//  StartView.swift
+//  Build Your Own (Team)
+//
+//  Created by Ethan Davis on 4/16/24.
+//
+
+import SwiftUI
+
+struct StartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StartView()
+}
