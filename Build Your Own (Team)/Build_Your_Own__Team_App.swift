120cbf8d17465ed1169444d85d1ac8377d3bda34
Ethan Davis <edavis@bsd220.org>
2024-04-16 10:05:30 -0500

    added startview

--- Build Your Own (Team)/Build_Your_Own__Team_App.swift
+++ Build Your Own (Team)/Build_Your_Own__Team_App.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Build_Your_Own__Team_App: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            StartView()
         }
     }
 }
--- Build Your Own (Team)/StartView.swift
+++ Build Your Own (Team)/StartView.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct StartView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            NavigationLink("Start", destination: ContentView())
+        }
     }
 }
 
