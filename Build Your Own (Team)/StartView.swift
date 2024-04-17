220182f03be4ecc2bb23f742d14471fbf8cb732f
Ethan Davis <edavis@bsd220.org>
2024-04-17 09:51:04 -0500

    almost done with title screen

--- Build Your Own (Team)/StartView.swift
+++ Build Your Own (Team)/StartView.swift
@@ -10,11 +10,31 @@ import SwiftUI
 struct StartView: View {
     var body: some View {
         NavigationView {
-            NavigationLink("Start", destination: ContentView())
+            ZStack {
+                Image("background")
+                    .resizable(resizingMode: .stretch)
+                    .aspectRatio(contentMode: .fill)
+                    .ignoresSafeArea()
+                VStack {
+                    CustomText(text: "Title (TBD)")
+                        .padding(60)
+                    NavigationLink(destination: ContentView(), label: {
+                        Text("Start")
+                    })
+                        .padding(100)
+                }
+            }
         }
     }
 }
 
 #Preview {
     StartView()
+}
+
+struct CustomText: View {
+    let text: String
+    var body: some View {
+        Text(text).font(Font.custom("Futura-Bold", size: 70))
+    }
 }
