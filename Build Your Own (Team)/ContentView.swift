597535f058a09aaee26a3a494b35b727cea8014b
Ethan <ethan.scott2@icloud.com>
2024-04-22 12:37:26 -0500

    Added the character jump method that allows the character to jump when clicked

--- Build Your Own (Team)/ContentView.swift
+++ Build Your Own (Team)/ContentView.swift
@@ -1,13 +1,16 @@
 import SwiftUI
 
 struct MovingBackground: View {
-    @State private var xOffset: CGFloat = 0
-    @State private var objPosition = 0
+    @State private var xOffset: CGFloat = 0 // Position of the background that increases every time the timer ticks
+    @State private var objPosition = 0 // Position of the block that increases every time the timer ticks
+    @State private var score = 0 // Will eventually be used to strore the score
+    
+    @State private var jumpOffset: CGFloat = 0 // New state variable to control the character's jump
     
     let timer = Timer.publish(every: 0.05, on: .main, in: .common).autoconnect() // Creates timer
     
     var body: some View {
-        GeometryReader { geometry in // This geometry reader privides info on the geometry of the photos inside of it. This helps us more easily resize the images
+        GeometryReader { geometry in // This geometry reader provides info on the geometry of the photos inside of it. This helps us more easily resize the images
             ZStack {
                 ForEach(0..<2) { index in
                     Image("background")
@@ -18,17 +21,21 @@ struct MovingBackground: View {
                 } // For loop for the moving background
                 
                 Rectangle()
-                    .fill(.red)
+                    .fill(Color.red)
                     .border(Color.black)
-                    .frame(width: 70, height: 70)
-                    .position(CGPoint(x: 890.0 + Double((objPosition)), y: 310.0))
-             //goes offscreen at x = - 35, so objPosition = -925
+                    .frame(width: 50, height: 50)
+                    .position(CGPoint(x: 890.0 + Double((objPosition)), y: 322.0)) // This is our test rectangle for now that can later be swapped out for obsticals. it moves across the screen and when it exits the left side, another one comes out the right side
                 
                 Image("Character") // May be changed in final game
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                     .frame(width: geometry.size.width * 0.3, height: geometry.size.height * 0.3) // This adjusts how far left or right the character is
-                    .offset(x: -geometry.size.width * 0.35, y: geometry.size.height * 0.29) // This adjusts the height of the character
+                    .offset(x: -geometry.size.width * 0.35, y: geometry.size.height * 0.29 + jumpOffset) // This adjusts the height of the character
+                    .gesture(
+                        TapGesture().onEnded {
+                            jump()
+                        } // When the character is tapped, it calls the jump function whih allows the character to jump
+                    )
             }
             .onReceive(timer) { _ in
                 xOffset -= 1 // Every time the timer ticks, it moves the background over towards the left, creating the effect that its moving
@@ -36,6 +43,8 @@ struct MovingBackground: View {
                 if objPosition <= -925 {
                     objPosition = 0
                 }
+                objPosition -= 7 // Moves object to the left every time the timer ticks
+                
                 if xOffset <= -geometry.size.width {
                     xOffset = 0
                 }
@@ -43,6 +52,18 @@ struct MovingBackground: View {
             .clipped()
         }
         .edgesIgnoringSafeArea(.all) // Makes it full screen
+    }
+    
+    func jump() {
+        withAnimation(.easeInOut(duration: 0.3)) { // Animation allows the character to smoothly jump on the screen
+            jumpOffset = -150 // How high the character jumps
+        }
+        // Dispatch queue is used to allow the jump to happen at the time of the click and finish executing 0.35 seconds after
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.35) { // How long the character is delayed in the air for.
+            withAnimation(.easeInOut(duration: 0.5)) { //how fast the character comes down
+                jumpOffset = 0
+            }
+        }
     }
 }
 
